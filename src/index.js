b80a12cf375396b50f51c883a8fb36c9cc9da2e7
AlexisMunozv <alexismunozv@icloud.com>
2018-06-01 10:10:50 -0500

    eliminado una variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 let string = document.getElementById("paraph").value;
 let offset = parseInt(document.getElementById("space").value);
-let string2 = cipher.encode(offset,string);
 
 let button1 = document.getElementById("encodeButton");
 button1.addEventListener("click", () => {

d9423957d427d8067404079cb8555be113cfc032
AlexisMunozv <alexismunozv@icloud.com>
2018-06-01 16:51:59 -0500

    Test corriendo al 80.95%

--- src/cipher.js
+++ src/cipher.js
@@ -1,46 +1,32 @@
 window.cipher = {
-      encode: function(offset, string){
-               
-        let stringCode='';
+      encode: (offset, string) => {
+       let stringCode='';
         for (let i=0; i<string.length; i++){
-         
-          let letterCodeASCI = string.toUpperCase().charCodeAt(i);
-         
+         let letterCodeASCI = string.toUpperCase().charCodeAt(i);
           if (letterCodeASCI === 32) {
               let emptySpace =' ';
               stringCode += emptySpace;
-           }
-
-          else{
+           } else {
             let letterCodeNumber = (letterCodeASCI-65 + offset)%26 + 65;
             let NewLetter = String.fromCharCode(letterCodeNumber);
             stringCode +=  NewLetter; 
-           }
-                                 
+           }                     
           }
           return stringCode;
        },
-      
-       decode: function(offset, string){  
+       decode: (offset, string) => {  
           let stringDecode = ' ';
-
           for(let i = 0; i<string.length; i++){
-         
             let letterCodeASCI = string.toUpperCase().charCodeAt(i);
-
                 if (letterCodeASCI ===32){
                   let emptySpace ='';
                   stringDecode += emptySpace;
-                 }
-
-                else{
+                 } else {
                   let letterCodeNumber = (letterCodeASCI-65 - offset)%26 + 65;
                   let NewLetter = String.fromCharCode(letterCodeNumber);
                   stringDecode +=  NewLetter; 
-                 }
-                               
+                 }               
           }
          return stringDecode;
-         } 
-     
+        }       
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 let string = document.getElementById("paraph");
 let offset = document.getElementById("space");
-
+//Funcion de Boton Encriptar
 let button1 = document.getElementById("encodeButton");
 button1.addEventListener("click", () => {
-    let callFunctionEncode = window.cipher.encode (parseInt(offset.value), string.value);
-    document.getElementById("text1").value = callFunctionEncode;
+let callFunctionEncode = window.cipher.encode (parseInt(offset.value), string.value);
+document.getElementById("text1").value = callFunctionEncode;
 } ); 
-
+//Funcion de boton Desencriptar
 let button2 = document.getElementById("decodeButton");
 button2.addEventListener("click", () => {
-    let callFunctionDecode = window.cipher.decode(parseInt(offset.value), string.value);
-    document.getElementById("text2").value = callFunctionDecode;
+let callFunctionDecode = window.cipher.decode(parseInt(offset.value), string.value);
+document.getElementById("text2").value = callFunctionDecode;
 } );
-
-
-
-
-
-

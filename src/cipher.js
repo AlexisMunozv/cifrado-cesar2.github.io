91ee54d75454afb8fb47006aa41cceb195b36d00
AlexisMunozv <alexismunozv@icloud.com>
2018-06-01 11:30:55 -0500

    Lee los espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,32 @@
 window.cipher = {
       encode: function(offset, string){
+        debugger
        let stringCode='';
        let NewLetter='';
        let letterCodeNumber=''
+       let emptySpace ='';
        
 
         for (let i=0; i<string.length; i++){
           let letterCodeASCI = string.toUpperCase().charCodeAt(i);
          
-          if (letterCodeASCI===32) {
-              stringcode='';
+          if (letterCodeASCI === 32) {
+              emptySpace=' ';
+              stringCode += emptySpace;
            }
-      
+
           else{
             letterCodeNumber=(letterCodeASCI-65 + offset)%26 + 65;
             NewLetter=String.fromCharCode(letterCodeNumber);
+            stringCode +=  NewLetter; 
            }
-           stringCode=stringCode+NewLetter;
-           
+                                 
           }
-          return stringCode
-       }
+          
+       },
       
-    
+       decode: function(offset, string){  
+       
+     
+     }
 }

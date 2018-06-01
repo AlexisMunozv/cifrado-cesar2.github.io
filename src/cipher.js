e9d9716c571c3130db96bbd3f6d1d481be78ad66
AlexisMunozv <alexismunozv@icloud.com>
2018-06-01 15:21:09 -0500

    Endoce y Decode funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,20 @@
 window.cipher = {
       encode: function(offset, string){
+        debugger
+         
         let stringCode='';
         for (let i=0; i<string.length; i++){
          
           let letterCodeASCI = string.toUpperCase().charCodeAt(i);
          
           if (letterCodeASCI === 32) {
-              let emptySpace ='';
+              let emptySpace =' ';
               stringCode += emptySpace;
            }
 
           else{
-            let letterCodeNumber=(letterCodeASCI-65 + offset)%26 + 65;
-            let NewLetter=String.fromCharCode(letterCodeNumber);
+            let letterCodeNumber = (letterCodeASCI-65 + offset)%26 + 65;
+            let NewLetter = String.fromCharCode(letterCodeNumber);
             stringCode +=  NewLetter; 
            }
                                  
@@ -21,13 +23,25 @@ window.cipher = {
        },
       
        decode: function(offset, string){  
-        let stringDecode='';
+          let stringDecode = ' ';
 
-         for(let i=0, i <string.length; i++){
+          for(let i = 0; i<string.length; i++){
          
-          let letterCodeASCI=
+            let letterCodeASCI = string.toUpperCase().charCodeAt(i);
 
-        } }
-     
+                if (letterCodeASCI ===32){
+                  let emptySpace ='';
+                  stringDecode += emptySpace;
+                 }
+
+                else{
+                  let letterCodeNumber = (letterCodeASCI-65 - offset)%26 + 65;
+                  let NewLetter = String.fromCharCode(letterCodeNumber);
+                  stringDecode +=  NewLetter; 
+                 }
+                               
+          }
+         return stringDecode;
+         } 
      
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ let offset = document.getElementById("space");
 
 let button1 = document.getElementById("encodeButton");
 button1.addEventListener("click", () => {
-    let callFunctionEncode = window.cipher.encode (parseInt(offset.value),string.value);
+    let callFunctionEncode = window.cipher.encode (parseInt(offset.value), string.value);
     document.getElementById("text1").value = callFunctionEncode;
 } ); 
 
 let button2 = document.getElementById("decodeButton");
 button2.addEventListener("click", () => {
-    let callFunctionDecode = window.cipher.decode (parseInt(offset.value), string.value);
+    let callFunctionDecode = window.cipher.decode(parseInt(offset.value), string.value);
     document.getElementById("text2").value = callFunctionDecode;
 } );
 

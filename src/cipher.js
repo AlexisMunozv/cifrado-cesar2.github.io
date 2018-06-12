fccfaab36bec0102b28e619281035513965dac52
AlexisMunozv <alexismunozv@icloud.com>
2018-06-12 02:20:43 -0500

    sin errores, por fin

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 window.cipher = {
       encode: (offset, string) => {
        let stringCode='';
-        for (let i=0; i<string.length; i++){
+         for (let i=0; i<string.length; i++){
          let letterCodeASCI = string.toUpperCase().charCodeAt(i);
           if (letterCodeASCI === 32) {
               let emptySpace =' ';
@@ -16,13 +16,13 @@ window.cipher = {
        },
        decode: (offset, string) => {  
           let stringDecode = ' ';
-          for(let i = 0; i<string.length; i++){
+           for(let i = 0; i<string.length; i++){
             let letterCodeASCI = string.toUpperCase().charCodeAt(i);
                 if (letterCodeASCI ===32){
                   let emptySpace =' ';
                   stringDecode += emptySpace;
                  } else {
-                  let letterCodeNumber = (letterCodeASCI-65 - offset)%26 + 65;
+                  let letterCodeNumber = (letterCodeASCI + 65 - offset)%26 + 65;
                   let NewLetter = String.fromCharCode(letterCodeNumber);
                   stringDecode +=  NewLetter; 
                  }               
